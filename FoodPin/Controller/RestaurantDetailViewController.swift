a138881c9dff84a905ab1f70c54f172fff680f56
Matthew Richardson <mat_richardson@mac.com>
2019-05-24 20:41:32 +1000

    Finished chapter 14

--- FoodPin/Controller/RestaurantDetailViewController.swift
+++ FoodPin/Controller/RestaurantDetailViewController.swift
@@ -8,7 +8,41 @@
 
 import UIKit
 
-class RestaurantDetailViewController: UIViewController {
+class RestaurantDetailViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.row {
+        case 0:
+            let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RestaurantDetailIconTextCell.self), for: indexPath) as! RestaurantDetailIconTextCell
+            cell.iconImageView.image = UIImage(named: "phone")
+            cell.shortTextLabel.text = restaurant.phone
+            cell.selectionStyle = .none
+            return cell
+            
+        case 1:
+            let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RestaurantDetailIconTextCell.self), for: indexPath) as! RestaurantDetailIconTextCell
+            cell.iconImageView.image = UIImage(named: "map")
+            cell.shortTextLabel.text = restaurant.location
+            cell.selectionStyle = .none
+            return cell
+        case 2:
+            let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RestaurantDetailTextCell.self), for: indexPath) as! RestaurantDetailTextCell
+            cell.descriptionLabel.text = restaurant.description
+            cell.selectionStyle = .none
+            return cell
+        default:
+            fatalError("Failed to instantiate the table view cell for detail view cont roller")
+        }
+    }
+    
 
     @IBOutlet var restaurantImageView: UIImageView!
     @IBOutlet var restaurantNameLabel: UILabel!
@@ -29,6 +63,11 @@ class RestaurantDetailViewController: UIViewController {
         headerView.nameLabel.text = restaurant.name
         headerView.typeLabel.text = restaurant.type
         headerView.heartImageView.isHidden = (restaurant.isVisited) ? false : true
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.separatorStyle = .none
     }
     
 
---
+++ FoodPin/View/RestaurantDetailIconTextCell.swift
@@ -0,0 +1,31 @@
+//
+//  RestaurantDetailIconTextCell.swift
+//  FoodPin
+//
+//  Created by Matthew Richardson on 24/5/19.
+//  Copyright © 2019 Matthew Richardson. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantDetailIconTextCell: UITableViewCell {
+
+    @IBOutlet var iconImageView: UIImageView!
+    @IBOutlet var shortTextLabel: UILabel! {
+        didSet {
+            shortTextLabel.numberOfLines = 0
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
